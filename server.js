4c735a5531299fc373632b24e22988c73626d09f
Shree Vishnu Sharma <svsharmamysuru@gmail.com>
2025-02-15 00:15:38 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,17 @@
 const express = require('express');
-const mongoose = require('mongoose');
 const path = require('path');
 const app = express();
 const port = 3000;
+const mongoose = require('mongoose');
+require('dotenv').config(); // Ensure dotenv is required if you're using a .env file
+
+mongoose.connect(process.env.MONGO_URI, { 
+    useNewUrlParser: true, 
+    useUnifiedTopology: true 
+})
+.then(() => console.log("MongoDB Connected"))
+.catch(err => console.error("MongoDB Connection Error:", err));
+
 
 mongoose.connect('mongodb://127.0.0.1:27017/physioBooking', {
     useNewUrlParser: true,
